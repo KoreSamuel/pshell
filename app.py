faf2c2f22db5be44e5647679ffe18181ad2dac6c
koresamuel <swustxiaojie@163.com>
2020-08-16 21:18:13 +0800

    add db

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Hello World!'
+    return "Hello World!"
 
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
+@app.route("/hello/")
+@app.route("/hello/<name>")
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    return render_template("hello.html", name=name)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.debug = True
     app.run()
